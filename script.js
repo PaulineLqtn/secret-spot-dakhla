611634e5d20894bc622874500acf7b3f549f5fd6
Pauline Lanquetin <paulinelanquetin@MacBook-Pro-de-Pauline.local>
2025-11-09 13:56:52 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// Ce script est facultatif, il est utilisé pour charger la photo de couverture en fonction de la taille de l'écran.
+window.addEventListener('scroll', function() {
+    var coverPhoto = document.querySelector('.cover-photo');
+    var navbar = document.querySelector('.navbar');
+    var navLinks = navbar.querySelectorAll('a');
+    //var navIcon = navbar.querySelectorAll('navbar-toggler-icon');
+
+    // Récupérer la hauteur de la photo de couverture
+    var coverPhotoHeight = coverPhoto.getBoundingClientRect().height;
+    var navbarHeight = navbar.getBoundingClientRect().height;
+
+    // Vérifier si l'utilisateur a fait défiler jusqu'au bas de la photo de couverture
+    if (window.scrollY >= coverPhotoHeight - navbarHeight) {
+        setTimeout(function() {
+            navbar.style.backgroundColor = 'rgba(244, 244, 244, 0.8)';
+            navbar.style.backdropFilter = 'blur(10px)';
+            navbar.classList.add('scrolled');
+            navLinks.forEach(function(link) {
+                link.style.color = '#373737';
+            });
+        }, 40);
+    }
+    else {
+        setTimeout(function() {
+            navbar.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+            navbar.style.backdropFilter = 'blur(0px)';
+            navbar.classList.remove('scrolled');
+            navLinks.forEach(function(link) {
+                link.style.color = '#FFF';
+            });
+        }, 40);
+        
+    }
+});
+
+
+// // Initialize and add the map
+// let map;
+
+// async function initMap() {
+//   // The location of Uluru
+//   const position = { lat: -25.344, lng: 131.031 };
+//   // Request needed libraries.
+//   //@ts-ignore
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+//   // The map, centered at Uluru
+//   map = new Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: position,
+//     mapId: "DEMO_MAP_ID",
+//   });
+
+//   // The marker, positioned at Uluru
+//   const marker = new AdvancedMarkerElement({
+//     map: map,
+//     position: position,
+//     title: "Uluru",
+//   });
+// }
+
+// initMap();
