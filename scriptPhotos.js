b929ca9d5cb7d9527e169e5747f2e5c06139c7d7
Pauline Lanquetin <paulinelanquetin@MacBook-Pro-de-Pauline.local>
2026-02-20 19:26:29 -0500

    Add galery photos

--- scriptPhotos.js
+++ scriptPhotos.js
@@ -1,18 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const images = [
-    "EF3E4630-0767-46F0-A2E1-EAB71A28F473_1_201_a.jpeg",
-    "20240211_111755.jpg",
-    "DSC_4739 2.jpg",
-    "EF3E4630-0767-46F0-A2E1-EAB71A28F473_1_201_a.jpeg",
-    "20240211_111755.jpg",
-    "DSC_4739 2.jpg",
-    "20240211_111755.jpg",
-    "DSC_4739 2.jpg"
+    "A7404314.webp",
+    "A7402125-2.webp",
+    "A7403416.webp",
+    "A7402653.webp",
+    "A7400736.webp",
+    "A7402636.webp",
+    "A7403320.webp",
+    "A7401224-2.webp",
+    "A7400792.webp",
+    "A7402853-2.webp",
+    "A7400870-2.webp",
+    "A7401995.webp",
+    "A7402885.webp",
+    "AP2A1901.webp",
+    "AP2A2068.webp",
+    "A7403411.webp",
+    "A7400838.webp",
+    "AP2A1921.webp",
+    "A7402319.webp",
+    "A7403324.webp",
+    "A7404390.webp",
+    "A7400848.webp",
+    "A7404228.webp",
+    "A7400832.webp",
+    "A7404442.webp",
+    "A7404354.webp",
+    "A7404386.webp",
+    "AP2A1965.webp",
+    "A7404341.webp",
+    "A7401583.webp",
+    "A7404263.webp",
+    "A7404250.webp",
+    "A7401345.webp",
+    "A7404410.webp",
+    "A7404383.webp",
+    "A7403332.webp",
+    "A7404049.webp",
+    "A7400986.webp",
+    "A7404052.webp",
+    "A7402193-2.webp",
+    "A7401246.webp",
+    "AP2A2032.webp",
+    "A7404105.webp",
+    "A7404338.webp",
+    "A7404191.webp",
+    "A7401808.webp",
+    "AP2A2029.webp",
+    "A7403810.webp"
   ];
 
   const gallery = document.getElementById("gallery");
-  const basePath = "ressources/images/";
+  const basePath = "https://secretspot-media.s3.eu-west-3.amazonaws.com/photos/";
 
   images.forEach(img => {
     gallery.insertAdjacentHTML("beforeend", `
