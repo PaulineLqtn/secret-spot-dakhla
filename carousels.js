4a5b3a51464c7a9b0c259fdbb42171ecd13b8731
Pauline Lanquetin <paulinelanquetin@MacBook-Pro-de-Pauline.local>
2026-03-01 18:14:16 -0800

    Update carousels

--- carousels.js
+++ carousels.js
@@ -5,6 +5,21 @@ document.querySelectorAll('[data-carousel]').forEach(carousel => {
   carousel.id = `carousel-${id}`
 
   carousel.innerHTML = `
+    <!-- INDICATEURS -->
+    <div class="carousel-indicators">
+      ${images.map((_, i) => `
+        <button
+          type="button"
+          data-bs-target="#carousel-${id}"
+          data-bs-slide-to="${i}"
+          class="${i === 0 ? 'active' : ''}"
+          aria-current="${i === 0 ? 'true' : 'false'}"
+          aria-label="Slide ${i + 1}">
+        </button>
+      `).join('')}
+    </div>
+
+    <!-- SLIDES -->
     <div class="carousel-inner">
       ${images.map((img, i) => `
         <div class="carousel-item ${i === 0 ? 'active' : ''}">
@@ -13,12 +28,13 @@ document.querySelectorAll('[data-carousel]').forEach(carousel => {
       `).join('')}
     </div>
 
-    <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${id}" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-    </button>
+    <!-- CONTROLES -->
+    <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${id}" data-bs-slide="prev"> -->
+    <!-- <span class="carousel-control-prev-icon"></span> -->
+    <!-- </button> -->
 
-    <button class="carousel-control-next" type="button" data-bs-target="#carousel-${id}" data-bs-slide="next">
-      <span class="carousel-control-next-icon"></span>
-    </button>
+    <!-- <button class="carousel-control-next" type="button" data-bs-target="#carousel-${id}" data-bs-slide="next"> -->
+    <!-- <span class="carousel-control-next-icon"></span> -->
+    <!-- </button> -->
   `
 })
