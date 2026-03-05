de3400c54bfc0874768c42bf8ae64d383aba39cd
Pauline Lanquetin <paulinelanquetin@MacBook-Pro-de-Pauline.local>
2026-03-05 15:49:32 -0800

    Fix mobile menu

--- script.js
+++ script.js
@@ -1,41 +1,6 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc =
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'
   
-// Ce script est facultatif, il est utilisé pour charger la photo de couverture en fonction de la taille de l'écran.
-window.addEventListener('scroll', function() {
-    var coverPhoto = document.querySelector('.cover-photo');
-    var navbar = document.querySelector('.navbar');
-    var navLinks = navbar.querySelectorAll('a');
-    //var navIcon = navbar.querySelectorAll('navbar-toggler-icon');
-
-    // Récupérer la hauteur de la photo de couverture
-    var coverPhotoHeight = coverPhoto.getBoundingClientRect().height;
-    var navbarHeight = navbar.getBoundingClientRect().height;
-
-    // Vérifier si l'utilisateur a fait défiler jusqu'au bas de la photo de couverture
-    if (window.scrollY >= coverPhotoHeight - navbarHeight) {
-        setTimeout(function() {
-            navbar.style.backgroundColor = 'rgba(244, 244, 244, 0.8)';
-            navbar.style.backdropFilter = 'blur(10px)';
-            navbar.classList.add('scrolled');
-            navLinks.forEach(function(link) {
-                link.style.color = '#373737';
-            });
-        }, 40);
-    }
-    else {
-        setTimeout(function() {
-            navbar.style.backgroundColor = 'rgba(255, 255, 255, 0)';
-            navbar.style.backdropFilter = 'blur(0px)';
-            navbar.classList.remove('scrolled');
-            navLinks.forEach(function(link) {
-                link.style.color = '#7A746A';
-            });
-        }, 40);
-        
-    }
-});
-
 const faqItems = document.querySelectorAll('.faq-item');
   faqItems.forEach(item => {
     item.querySelector('.faq-question').addEventListener('click', () => {
